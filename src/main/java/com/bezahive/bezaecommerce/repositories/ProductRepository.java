87dd585426574c4be40f454d83c511bdd00e90ae
Zabdiel Elias <zabdieljr2@gmail.com>
2023-11-15 17:20:16 -0500

    findByCategoryId method test on rest api

--- src/main/java/com/bezahive/bezaecommerce/repositories/ProductRepository.java
+++ src/main/java/com/bezahive/bezaecommerce/repositories/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.bezahive.bezaecommerce.repositories;
 
 import com.bezahive.bezaecommerce.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@CrossOrigin("http://localhost:4200")
@@ -9,5 +12,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // add find by category id pageable @Param("id") Long id, Pageable pageable
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
 
 }
