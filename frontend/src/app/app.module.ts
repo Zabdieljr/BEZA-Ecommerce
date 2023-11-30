907cf92054a549a09417d867d653339c893e289d
Zabdiel Elias <zabdieljr2@gmail.com>
2023-11-29 22:03:45 -0500

    Security order-history frontend with Okta and springboot 3.0.0 closes #27

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,103 +1,111 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {Injector, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from './services/product.service';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ProductService} from './services/product.service';
 
 import {Routes, RouterModule, Router} from '@angular/router';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
+import {SearchComponent} from './components/search/search.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
 // add import ngb module
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {CartStatusComponent} from './components/cart-status/cart-status.component';
+import {CartDetailsComponent} from './components/cart-details/cart-details.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { LoginComponent } from './components/login/login.component';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {LoginComponent} from './components/login/login.component';
+import {LoginStatusComponent} from './components/login-status/login-status.component';
 
 // add Okta Auth imports
 
 import {
-  OktaAuthModule,
-  OktaCallbackComponent,
-  OKTA_CONFIG, OktaAuthGuard
+    OktaAuthModule,
+    OktaCallbackComponent,
+    OKTA_CONFIG, OktaAuthGuard
 } from '@okta/okta-angular';
 
-import { OktaAuth } from '@okta/okta-auth-js';
+import {OktaAuth} from '@okta/okta-auth-js';
 
 import myAppConfig from './config/my-app-config';
-import { MembersPageComponent } from './components/members-page/members-page.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import {MembersPageComponent} from './components/members-page/members-page.component';
+import {OrderHistoryComponent} from './components/order-history/order-history.component';
+import {AuthInterceptorService} from "./services/auth-interceptor.service";
 
 const oktaConfig = myAppConfig.oidc;
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
 function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
-  // Use injector to access any service available within your application
-  const router = injector.get(Router);
+    // Use injector to access any service available within your application
+    const router = injector.get(Router);
 
-  // Redirect the user to your custom login page
-  router.navigate(['/login']);
+    // Redirect the user to your custom login page
+    router.navigate(['/login']);
 }
 
 
 const routes: Routes = [
-  {path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard],
-    data: {onAuthRequired: sendToLoginPage} },
-  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
-    data: {onAuthRequired: sendToLoginPage} },
-  {path: 'login/callback', component: OktaCallbackComponent},
-  {path: 'login', component: LoginComponent},
+    {
+        path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard],
 
-  //add route for checkout
-  {path: 'checkout', component: CheckoutComponent},
-  //add route for cart details
-  {path: 'cart-details', component: CartDetailsComponent},
-  //add product details  component
-  {path: 'products/:id', component: ProductDetailsComponent},
-  //add a route for search by name
-  {path: 'search/:keyword', component: ProductListComponent},
-  {path: 'category/:id/:name', component: ProductListComponent},
-  {path: 'category', component: ProductListComponent},
-  {path: 'products', component: ProductListComponent},
-  {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+        data: {onAuthRequired: sendToLoginPage}
+    },
+    {
+        path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
+        data: {onAuthRequired: sendToLoginPage}
+    },
+    {path: 'login/callback', component: OktaCallbackComponent},
+    {path: 'login', component: LoginComponent},
+
+    //add route for checkout
+    {path: 'checkout', component: CheckoutComponent},
+    //add route for cart details
+    {path: 'cart-details', component: CartDetailsComponent},
+    //add product details  component
+    {path: 'products/:id', component: ProductDetailsComponent},
+    //add a route for search by name
+    {path: 'search/:keyword', component: ProductListComponent},
+    {path: 'category/:id/:name', component: ProductListComponent},
+    {path: 'category', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductCategoryMenuComponent,
-    SearchComponent,
-    ProductDetailsComponent,
-    CartStatusComponent,
-    CartDetailsComponent,
-    CheckoutComponent,
-    LoginComponent,
-    LoginStatusComponent,
-    MembersPageComponent,
-    OrderHistoryComponent,
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ProductCategoryMenuComponent,
+        SearchComponent,
+        ProductDetailsComponent,
+        CartStatusComponent,
+        CartDetailsComponent,
+        CheckoutComponent,
+        LoginComponent,
+        LoginStatusComponent,
+        MembersPageComponent,
+        OrderHistoryComponent,
 
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    HttpClientModule,
-    //add ngb module
-    NgbModule,
-    // add support for reactive forms
-    ReactiveFormsModule,
-    // add OktaAuthModule
-    OktaAuthModule
+    ],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        HttpClientModule,
+        //add ngb module
+        NgbModule,
+        // add support for reactive forms
+        ReactiveFormsModule,
+        // add OktaAuthModule
+        OktaAuthModule
 
-  ],
-  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth }}],
-  bootstrap: [AppComponent]
+    ],
+    providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}}, {provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService, multi: true}],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
